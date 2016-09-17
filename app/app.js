9a7547a8c79a7ed4f1b08b062b604588724a8052
Andy <chen1370@purdue.edu>
2016-09-17 18:53:25 +0000

    Postgres init in global with RESTful

--- app/app.js
+++ app/app.js
@@ -9,7 +9,6 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-var pg = require('pg');
 
 
 // view engine setup
@@ -67,53 +66,8 @@ app.use(function(err, req, res, next) {
 // and client options
 // note: all config is optional and the environment variables
 // will be read if the config is not present
-var config = {
-  user: 'postgres', //env var: PGUSER
-  database: 'pecdb', //env var: PGDATABASE
-  password: 'postgres', //env var: PGPASSWORD
-  host: 'localhost', // Server hosting the postgres database
-  port: 5432, //env var: PGPORT
-  max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-};
-
-
-//this initializes a connection pool
-//it will keep idle connections open for a 30 seconds
-//and set a limit of maximum 10 idle clients
-var pool = new pg.Pool(config);
-
-// to run a query we can acquire a client from the pool,
-// run a query on the client, and then return the client to the pool
-pool.connect(function(err, client, done) {
-  if(err) {
-    return console.error('error fetching client from pool', err);
-  }
-  //client.query("insert into equipment (EQUIPMENT_NAME,EQUIPMENT_TYPE,EQUIPMENT_BRAND,EQUIPMENT_DESCRIPTION) values ('Blue Wrench','Wrench','Toyota','A blue wrench');",  function(err, result) {
-      
-  client.query("delete from equipment where equipment_unique_id = 1;",  function(err, result) {
-    //call `done()` to release the client back to the pool
-    done();
-
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result);
-    //output: 1
-  });
-});
-
-pool.on('error', function (err, client) {
-  // if an error is encountered by a client while it sits idle in the pool
-  // the pool itself will emit an error event with both the error and
-  // the client which emitted the original error
-  // this is a rare occurrence but can happen if there is a network partition
-  // between your application and the database, the database restarts, etc.
-  // and so you might want to handle it and at least log it out
-  console.error('idle client error', err.message, err.stack)
-})
 
 
 module.exports = app;
 
-app.listen(8080);
+app.listen(parseInt(process.argv[2]));
