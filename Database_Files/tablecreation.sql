a17f3516a920662a2e11f72cf66fab6fab2ba924
Hongda Zeng <hongda.code@gmail.com>
2016-09-19 12:47:12 +0000

    Update tablecreation.sql add table for login

--- Database_Files/tablecreation.sql
+++ Database_Files/tablecreation.sql
@@ -1,18 +1,15 @@
 /*
 TO RUN
-psql -U postgres -d pecdb -a -f my_script.sql
-
+psql -U postgres -d pecdb -a -f my_script.sq
 */
 
 CREATE TABLE EQUIPMENT(
   EQUIPMENT_UNIQUE_ID serial primary key not null,
-  
   EQUIPMENT_NAME varchar,
   EQUIPMENT_TYPE varchar,
   EQUIPMENT_BRAND varchar,
   EQUIPMENT_DESCRIPTION varchar,
   EQUIPMENT_IMAGEURL varchar default 'http://www.drodd.com/images15/red-x4.png',
-  
   EQUIPMENT_ENTRYUSER varchar,
   EQUIPMENT_DATEADDED timestamp default current_timestamp
  );
@@ -31,7 +28,18 @@ CREATE TABLE EQUIPMENT(
    LOG_ENTRYID serial primary key not null,
    LOG_ENTRYDATE timestamp default current_timestamp,
    LOG_USERID int,
-   LOG_CHECKINGOUT boolean,
+   LOG_ISCHECKINGOUT boolean,
    LOG_EQUIPMENTID int,
    LOG_EQUIPMENTCONDITION int
+ );
+ 
+ CREATE TABLE LOGIN(
+   LOGIN_ENTRYID serial primary key not null,
+   LOGIN_ENTRYDATE timestamp default current_timestamp,
+   LOGIN_USERNAME varchar,
+   LOGIN_PASSWORD varchar,
+   LOGIN_PHONE bigint,
+   LOGIN_EMAIL varchar,
+   LOGIN_SECRETQUESTION varchar,
+   LOGIN_SECRETANSWER varchar
  );
