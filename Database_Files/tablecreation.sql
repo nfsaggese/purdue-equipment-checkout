2e0c976f366eea3c0dec9580e47b221aaad091f6
Hongda Zeng <hongda.code@gmail.com>
2016-09-19 12:51:12 +0000

    Update tablecreation.sql add protection against null input

--- Database_Files/tablecreation.sql
+++ Database_Files/tablecreation.sql
@@ -5,7 +5,7 @@ psql -U postgres -d pecdb -a -f my_script.sq
 
 CREATE TABLE EQUIPMENT(
   EQUIPMENT_UNIQUE_ID serial primary key not null,
-  EQUIPMENT_NAME varchar,
+  EQUIPMENT_NAME varchar not null,
   EQUIPMENT_TYPE varchar,
   EQUIPMENT_BRAND varchar,
   EQUIPMENT_DESCRIPTION varchar,
@@ -16,9 +16,9 @@ CREATE TABLE EQUIPMENT(
  
  CREATE TABLE USERS(
   USERS_UNIQUE_ID serial primary key not null,
-  USERS_FIRSTNAME varchar,
-  USERS_LASTNAME varchar,
-  USERS_ISADMIN boolean,
+  USERS_FIRSTNAME varchar not null,
+  USERS_LASTNAME varchar not null,
+  USERS_ISADMIN boolean not null,
   USERS_DESCRIPTION varchar,
   USERS_ENTRYUSER varchar,
   USERS_DATEADDED timestamp default current_timestamp
@@ -27,17 +27,17 @@ CREATE TABLE EQUIPMENT(
  CREATE TABLE LOG(
    LOG_ENTRYID serial primary key not null,
    LOG_ENTRYDATE timestamp default current_timestamp,
-   LOG_USERID int,
-   LOG_ISCHECKINGOUT boolean,
-   LOG_EQUIPMENTID int,
+   LOG_USERID int not null,
+   LOG_ISCHECKINGOUT boolean not null,
+   LOG_EQUIPMENTID int not null,
    LOG_EQUIPMENTCONDITION int
  );
  
  CREATE TABLE LOGIN(
    LOGIN_ENTRYID serial primary key not null,
    LOGIN_ENTRYDATE timestamp default current_timestamp,
-   LOGIN_USERNAME varchar,
-   LOGIN_PASSWORD varchar,
+   LOGIN_USERNAME varchar not null,
+   LOGIN_PASSWORD varchar not null,
    LOGIN_PHONE bigint,
    LOGIN_EMAIL varchar,
    LOGIN_SECRETQUESTION varchar,
