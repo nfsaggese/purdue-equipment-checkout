a86741338b3c8b80988afcbaa5d267ce06bcccf4
Karmatize <karmatizej@gmail.com>
2016-09-29 15:19:56 -0400

    Sign up page adjusted

---
+++ static_ui/src/js/sign.js
@@ -0,0 +1,24 @@
+
+function submitLogin(){
+  var theUrl = "http://ec2-52-42-46-135.us-west-2.compute.amazonaws.com:8080/loginUser?USERS_EMAIL="+document.getElementById("email").value+"&USERS_PASSWORD="+document.getElementById("password").value;
+
+  $.get(theUrl, function(data){//getDevices from the server
+    console.log(data);
+    var info = JSON.parse(data);
+    number = info['rows'];
+    if(number.length == 1){
+      sessionStorage.setItem("email", document.getElementById("email").value);
+      window.location = "index.html";
+    }
+    else{
+
+    }
+  });
+}
+
+$(document).ready(function(){
+  $('#submit').click(function(){
+    console.log("called");
+    submitLogin();
+  });
+});
