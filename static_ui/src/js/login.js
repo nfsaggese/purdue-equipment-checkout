1cbbf338e014b3a54e4e3d9b6438abad72fc1298
Karmatize <karmatizej@gmail.com>
2016-09-29 17:15:59 -0400

    changed unnecessary changes

--- static_ui/src/js/login.js
+++ static_ui/src/js/login.js
@@ -1,6 +1,6 @@
 
 function submitLogin(){
-  var theUrl = "http://ec2-52-42-46-135.us-west-2.compute.amazonaws.com:8080/loginUser?USERS_USERNAME="+document.getElementById("username").value+"&USERS_PASSWORD="+document.getElementById("password").value;
+  var theUrl = "http://ec2-52-42-46-135.us-west-2.compute.amazonaws.com:8080/loginUser?USERS_EMAIL="+document.getElementById("email").value+"&USERS_PASSWORD="+document.getElementById("password").value;
 
   $.get(theUrl, function(data){//getDevices from the server
     console.log(data);
