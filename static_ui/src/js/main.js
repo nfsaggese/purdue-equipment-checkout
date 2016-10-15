ad50fab268d1ef23b9ba65eb17e68b300898063a
Nick Saggese <nfsaggese@gmail.com>
2016-10-14 20:59:30 -0400

    fixed init view logic

--- static_ui/src/js/main.js
+++ static_ui/src/js/main.js
@@ -1,13 +1,16 @@
-var root = 'http://ec2-52-42-46-135.us-west-2.compute.amazonaws.com:8080'
+var root = 'http://ec2-52-42-46-135.us-west-2.compute.amazonaws.com:8080';
 
 //todo create back and forwards variables stacks with closures to store prior and future views
 
 //init
 $(document).ready(function() {
     var currentPage = window.location.pathname;
-    if(currentPage === '/index.html'){
+    console.log(currentPage);
+    if((currentPage == '/') || (currentPage == '/index.html')){
+      console.log('getting here');
       userGlobalNav();
     }else{
+      console.log('getting here');
       adminGlobalNav();
     }
 });
