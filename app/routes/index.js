f9905df0a81450aa2c1109d6ab9bd3d01152e863
Brian Chan <chan96>
2016-10-16 19:48:07 +0000

    add getuserinfo route

--- app/routes/index.js
+++ app/routes/index.js
@@ -34,6 +34,25 @@ router.get('/createUser', function(req, res, next) {
   });
 });
 
+router.get('/getUserInfo', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  global.postPool.connect(function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    var email = req.query.USERS_EMAIL;
+    client.query("SELECT * FROM users where users_email='" + email + "';", function(err, result) {
+      //call `done()` to release the client back to the pool
+      res.send(JSON.stringify(result, null, 2));
+      done();
+      if(err) {
+        return console.error('error running query', err);
+      }
+    //output: 1
+    });
+  });
+});
+
 /* GET home page. */
 router.get('/getDevices', function(req, res, next) {
   //res.render('index', { title: 'Express' });
