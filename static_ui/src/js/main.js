0812caa5ced1bfa3eef648d5b281e3ec7377948e
Nick Saggese <nfsaggese@gmail.com>
2016-10-16 14:09:03 -0400

    updated view

--- static_ui/src/js/main.js
+++ static_ui/src/js/main.js
@@ -6,7 +6,7 @@ var root = 'http://ec2-52-42-46-135.us-west-2.compute.amazonaws.com:8080';
 $(document).ready(function() {
     var currentPage = window.location.pathname;
     console.log(currentPage);
-    if((currentPage == '/') || (currentPage == '/index.html')){
+    if((currentPage == '/') || (currentPage == '/user.html')){
       console.log('getting here');
       userGlobalNav();
     }else{
