e732fabf1a1fd1e2900fd59a7a999ed6f64533fe
Nick Saggese <nfsaggese@gmail.com>
2016-10-21 13:56:51 -0400

    test

--- static_ui/src/js/main.js
+++ static_ui/src/js/main.js
@@ -1,4 +1,5 @@
 var root = 'http://ec2-52-42-46-135.us-west-2.compute.amazonaws.com:8080';
+var webroot = 'http://ec2-52-42-46-135.us-west-2.compute.amazonaws.com'
 //todo create back and forwards variables stacks with closures to store prior and future views
 
 //init
@@ -25,6 +26,9 @@ function evaluateCookie(data){
   data = JSON.parse(data);
   console.log("this is the response to check cookie");
   console.log(data);
+  if(data == false){
+    window.location.assign(window.location.hostname);
+  }
 }
 /* todo
 
