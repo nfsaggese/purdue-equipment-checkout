722b8644654de83efd22a5bc05aeaea36efe1cf5
Nick Saggese <nfsaggese@gmail.com>
2016-10-21 11:42:01 -0400

    test

--- static_ui/src/js/item.js
+++ static_ui/src/js/item.js
@@ -18,7 +18,7 @@ function displayItemHistory(data){
     var log = data['rows'];
     console.log(log);
     clearBottom();
-    if(log == NULL){
+    if(log === undefined){
       $('#pageBody').html('<h1>This item has no history.</h1>');
     }else{
       var adminEquipmentContainerLogTemplate = Handlebars.compile($('#admin-equipment-container-log').html());
