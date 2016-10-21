9e9de1dc99ee99839ec913c4c2f570a9c92ba8af
Nick Saggese <nfsaggese@gmail.com>
2016-10-21 13:11:36 -0400

    test

--- static_ui/src/js/user.js
+++ static_ui/src/js/user.js
@@ -1,3 +1,14 @@
 //USER FUNCTIONS
 function defaultUserProfileView(){}
-function defaultUserHistoryView(){}
+function defaultUserHistoryView(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("GET", root+'/getUserLog', true);
+  xhttp.onload = function(e){displayUserHistory(xhttp.responseText)};
+  xhttp.withCredentials = true;
+  xhttp.send(null);
+}
+function displayUserHistory(data){
+  console.log(data);
+  data = JSON.parse(data);
+  console.log(data);
+}
