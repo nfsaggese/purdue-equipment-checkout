262b55058e5c067d5ab9d6f6319bef5025634bd4
Brian Chan <chan96>
2016-10-27 01:08:47 +0000

    fix again

--- app/routes/index.js
+++ app/routes/index.js
@@ -95,7 +95,7 @@ router.post('/getCheckedOutItems', function(req, res, next) {
 	    var userID = userAuth.getUserID(req.cookies.token);
 	    var query = `select * from log l inner join users u on l.LOG_USERID = u.USERS_UNIQUE_ID
 	    inner join equipment e on e.equipment_unique_id = l.LOG_EQUIPMENTID where
-	    e.equipment_ischeckedout = false and l.LOG_USERID = {$userID};`;
+	    e.equipment_ischeckedout = false and l.LOG_USERID = ${userID};`;
 
 	    console.log('update log query: ' + query);
 
