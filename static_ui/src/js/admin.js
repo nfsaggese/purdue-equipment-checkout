be95752eda15fd8b904d345dbeaf99bea639b701
Xu Han <karmatizej@gmail.com>
2016-10-28 13:03:59 -0400

    Changed Nicks Code

--- static_ui/src/js/admin.js
+++ static_ui/src/js/admin.js
@@ -2,13 +2,12 @@ if(document.cookie.token){
   var xxhttp = new XMLHttpRequest();
   xxhttp.open("GET", root+'/getUserAdminLogin?USERID=1', true);
   xxhttp.onload = function (e){
+    console.log(xxhttp.responseText);
     if(xxhttp.responseText === "invalid cookie"){
       window.location = "user.html";
     }
   };
 }
-
-
 
 
 function defaultActionsView(){
