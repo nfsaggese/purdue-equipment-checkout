5e1440f30662a0dafa8c9177b4a67339cfde9d0f
Brian Chan <chan96>
2016-10-28 20:48:31 +0000

    getcheckedoutitems now has no duplicates

--- app/routes/index.js
+++ app/routes/index.js
@@ -93,10 +93,16 @@ router.post('/getCheckedOutItems', function(req, res, next) {
 		return;
 	    }
 	    var userID = userAuth.getUserID(req.cookies.token);
-	    var query = `select e.equipment_name, e.equipment_unique_id from log l inner join users u on l.LOG_USERID = u.USERS_UNIQUE_ID
+	    var query = `select e.equipment_name,e.equipment_ischeckedout,max(l.log_entryid) from
+	    log l inner join users u on l.LOG_USERID = u.USERS_UNIQUE_ID inner join equipment e on
+	    e.equipment_unique_id = l.LOG_EQUIPMENTID where e.equipment_ischeckedout = true and
+	    l.LOG_USERID = ${userID} group by
+	    e.equipment_name,e.equipment_ischeckedout;`;
+
+	    /*var query = `select e.equipment_name, e.equipment_unique_id from log l inner join users u on l.LOG_USERID = u.USERS_UNIQUE_ID
 	    inner join equipment e on e.equipment_unique_id = l.LOG_EQUIPMENTID where
 	    e.equipment_ischeckedout = true and l.LOG_USERID = ${userID};`;
-
+	    */
 	    console.log('update log query: ' + query);
 
 	    client.query(query, function(err, result) {
