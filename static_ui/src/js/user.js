6d6d194720d53b855972f0fe72fb3216c715e27a
Nick Saggese <nfsaggese@gmail.com>
2016-10-29 14:21:43 -0400

    added pfile iframe

--- static_ui/src/js/user.js
+++ static_ui/src/js/user.js
@@ -1,7 +1,7 @@
 //USER FUNCTIONS
 function defaultUserProfileView(){
   clearBottom();
-  $('#pageBody').html('<iframe src="/profile.html"></iframe>');
+  $('#pageBody').html('<iframe frameBorder="0" onload="this.width=screen.width;this.height=screen.height; src="/profile.html"></iframe>');
 }
 function defaultUserHistoryView(){
   var xhttp = new XMLHttpRequest();
